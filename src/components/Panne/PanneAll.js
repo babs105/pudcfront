a07b0f060823be4dad5736b140df35e442852b67
babs105 <mamembaye105@live.fr>
2021-03-13 17:59:06 +0000

    refresh list9

--- src/components/Panne/PanneAll.js
+++ src/components/Panne/PanneAll.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
 
-import { comiteService } from "../../service/comiteService";
 
 import { useHistory } from "react-router-dom";
 import Pagination from "../DataTable/Pagination";
-import { equipementService } from "../../service/equipementService";
 import { panneService } from "../../service/panneService";
 
 function PanneAll() {
@@ -106,7 +104,40 @@ function PanneAll() {
               Ajouter Comité
             </button>
           </div> */}
-          <table className="table-auto rounded-lg shadow w-full border-collapse">
+         {loading ? (
+                 
+                 
+                 <div className=" text-green-600 text-xl bg-gray-50  w-full flex flex-row justify-center items-center 
+                 rounded-lg py-3 px-12 focus:outline-none">
+                     <svg
+                className="animate-spin -ml-1 mr-3 h-10 w-10 "
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+              Chargement...
+              
+            
+         
+              
+            </div>
+            
+            
+          ):   <table className="table-auto rounded-lg shadow w-full border-collapse">
             <thead>
               <tr className="uppercase bg-gray-300">
                 <th className="py-4 px-2 border-r-2 border-grey-800">N°</th>
@@ -208,7 +239,7 @@ function PanneAll() {
                   </tr>
                 ))}
             </tbody>
-          </table>
+          </table>}
         </div>
         <Pagination
           nombreTotal={search(pannes).length}
