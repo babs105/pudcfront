0a203d51daa7adc23779d317277ff0a2d7982e08
babs105 <mamembaye105@live.fr>
2021-03-20 23:28:39 +0000

    refresh list17

--- src/components/Comite/ComiteList.js
+++ src/components/Comite/ComiteList.js
@@ -175,43 +175,45 @@ function ComiteList() {
                         <td className="py-4 px-2 border-b border-grey-light ">
                           {row.region}
                         </td>
-                        <td className=" flex justify-around items-center py-5 px-2 border-b border-grey-light">
-                          <svg
-                            onClick={() => {
-                              console.log(row.id);
-                              history.push(`/comite-show/${row.id}`);
-                            }}
-                            className="w-5 h-5 cursor-pointer"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
-                              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                            />
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
-                              d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                            />
-                          </svg>
-                          <svg
-                            onClick={() => {
-                              console.log(row.id);
-                              history.push(`/comite-edit/${row.id}`);
-                            }}
-                            className="w-5 h-5 cursor-pointer"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                          </svg>
+                        <td className="  py-4 px-2 border-b border-grey-light">
+                          <div className=" flex justify-around items-center">
+                            <svg
+                              onClick={() => {
+                                console.log(row.id);
+                                history.push(`/comite-show/${row.id}`);
+                              }}
+                              className="w-5 h-5 cursor-pointer"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                              />
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                              />
+                            </svg>
+                            <svg
+                              onClick={() => {
+                                console.log(row.id);
+                                history.push(`/comite-edit/${row.id}`);
+                              }}
+                              className="w-5 h-5 cursor-pointer"
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                            >
+                              <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                            </svg>
+                          </div>
                         </td>
                       </tr>
                     ))}
